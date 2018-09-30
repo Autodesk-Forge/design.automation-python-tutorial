675c731efb02707c2a0f95ce9303e92ada1e3d2f
xiaodongliang <xiaodongliang@hotmail.com>
2018-09-30 12:06:22 +0800

    update readme, file name and thumbnail

--- acad-da.py
+++ acad-da.py
@@ -1,25 +1,25 @@
 import requests #http://requests.readthedocs.org/en/latest/
 import argparse
 
-def getToken(consumerKey, consumerSecret):
-    """Obtain Apiggee token given a consumer key/secret"""
-    req = { 'client_id' : consumerKey, 'client_secret': consumerSecret, 'grant_type' : 'client_credentials','scope':'code:all'}
+def getToken(client_id, client_secret):
+    """Obtain Forge token given a client id & secret"""
+    req = { 'client_id' : client_id, 'client_secret': client_secret, 'grant_type' : 'client_credentials','scope':'code:all'}
     resp = requests.post('https://developer.api.autodesk.com/authentication/v1/authenticate', req).json();
     return resp['token_type'] + " " + resp['access_token']
 
 def printReport(args):
     """Download and print the AutoCAD.IO report for a given workitem"""
-    token = getToken(args.consumerKey, args.consumerSecret)
+    token = getToken(args.client_id, args.client_secret)
     target = 'https://developer.api.autodesk.com/autocad.io/us-east/v2/'
-    resp = requests.get("{0}/WorkItems('{1}')".format(target, args.workItem), headers={'Authorization': token}).json();
+    resp = requests.get("{0}/WorkItems('{1}')".format(target, args.workitem_id), headers={'Authorization': token}).json();
     resp = requests.get(resp['StatusDetails']['Report']);
     print(resp.text)
 
 #TODO: add support for other operations (submit workitem, query activities etc.)
-parser = argparse.ArgumentParser(description='Get error report from AutoCAD.IO for a work item.')
-parser.add_argument('--consumerKey', required=True);
-parser.add_argument('--consumerSecret', required=True);
-parser.add_argument('--workItem', required=True);
+parser = argparse.ArgumentParser(description='Get error report from AutoCAD Design Automation for a work item.')
+parser.add_argument('--client_id', required=True);
+parser.add_argument('--client_secret', required=True);
+parser.add_argument('--workitem_id', required=True);
 
 args = parser.parse_args()
 
